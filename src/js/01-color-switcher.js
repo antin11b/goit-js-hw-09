1e57414bf1c74b6903f936f9b9b292b303fd5077
Anton Bershak <anton.bershak@gmail.com>
2022-02-20 23:52:20 +0200

    Add Task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onClickStartBtn = () => {
+  startBtnRef.disabled = true;
+  stopBtnRef.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const onClickStopBtn = () => {
+  startBtnRef.disabled = false;
+  stopBtnRef.disabled = true;
+  clearInterval(timerId);
+};
+
+startBtnRef.addEventListener('click', onClickStartBtn);
+stopBtnRef.addEventListener('click', onClickStopBtn);
